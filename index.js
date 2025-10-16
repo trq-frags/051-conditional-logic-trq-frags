2c678097750faaa02a43c464d0a1e6d220da44b4
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-16 16:51:53 +0000

    code.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function validateAge(oldEnough){
-  // add your code here
+  if (oldEnough) {
+    return "old enough";
+  } else {
+    return "not old enough";
 }
+}
+console.log(validateAge(true)); 
+console.log(validateAge(false));
+console.log(validateAge(5 > 10)); 
+console.log(validateAge(10 > 5));
